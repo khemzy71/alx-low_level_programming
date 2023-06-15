b67311ad755d569cc5c723194d9aaa8e94d3143b
Michael kemisola <bolajiprincess711@gmail.com>
2023-06-15 08:58:52 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,14 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual puzzle,
- * followed by a new line.
- * Return: Always 0 (Success)
+ * main - prints "Programming is like building a multilingual
+ * puzzle,followed by a new line.
+ * Return: Always 0.
  */
 int main(void)
+
 {
 	puts("\"programming is like building a multilingual puzzle");
     return (0);
+
 }
