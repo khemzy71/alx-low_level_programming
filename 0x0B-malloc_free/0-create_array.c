179802111402ff92e703338dcae8172e6a073722
Michael kemisola <bolajiprincess711@gmail.com>
2023-07-10 09:48:30 -0700

    ./a

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * create_array -  creates an array of chars, and initializes it with a specific char
+ * create_array - creates an array of chars and initializes it with a specific char
  * @size: size of array
  * @c: fill array values with char
  * Return: pointer to the array
