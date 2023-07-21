7079c688b9edac2ce1308707b9c2e5a7fcb74c13
Michael kemisola <bolajiprincess711@gmail.com>
2023-07-21 10:59:01 -0700

    0-print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * print_name -  prints a name
+ * @name: parameter that function ptr needs
+ * @f: function to execute
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
