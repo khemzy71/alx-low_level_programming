3525a4dc892d6605bc642a050e208049fd1c8a67
Michael kemisola <bolajiprincess711@gmail.com>
2023-09-12 12:41:34 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
+ * struct dog - structure for dogs
+ * @name: first person
+ * @age: second person
+ * @owner: third person
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
