0d18d33279ee89d8912af949f6db97ca45a2d019
Michael kemisola <bolajiprincess711@gmail.com>
2023-09-13 19:06:22 -0700

    calc

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - add
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of the division
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
