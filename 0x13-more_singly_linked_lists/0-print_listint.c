c65ff2fbb98495ca2531059106fc93f833949321
Michael kemisola <bolajiprrincess711@gmail.com>
2023-09-25 12:03:07 -0700

    print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t num_nodes = 0;
-	
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		
+
 		num_nodes += 1;
 		h = h->next;
 	}
-	
+
 	return (num_nodes);
 }
